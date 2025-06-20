fd4dc3ab196386cec6b4108ec0c13ef644cb0bcb
fahad-inabia <fahad@inabia.com>
2025-06-20 12:41:15 +0500

    fix : requirements.txt

--- app/controllers/bot_controller.py
+++ app/controllers/bot_controller.py
@@ -1,15 +1,21 @@
-from fastapi import Query
+from fastapi import Query, HTTPException
 from app.schemas import BaseResponse, UserQuery
 from app.services import BotService
+
 
 class BotController:
     def __init__(self, bot_service: BotService):
         self.bot_service = bot_service
 
     async def ask_user(self, _schema: UserQuery) -> BaseResponse:
-        result = await self.bot_service.query_confluence(_schema.query)
-        
-        return BaseResponse(
-            message="success",
-            data=result
-        )
+        try:
+            result = await self.bot_service.query_confluence(_schema.query)
+
+            return BaseResponse(message="success", data=result)
+        except Exception as e:
+            print(e)
+            return BaseResponse(
+                message="error",
+                data=None,
+                error=str(e),
+            )
